8c3e8504f076a9aa47ad9e1778bffeac7110fb4f
AnmolKumar <anmolgkv@gmail.com>
2022-11-27 11:16:39 +0530

    Added logic to create local function

--- src/commands/function/create.ts
+++ src/commands/function/create.ts
@@ -1,7 +1,10 @@
 import * as os from 'os';
-import { flags, SfdxCommand } from '@salesforce/command';
-import { Messages, SfError } from '@salesforce/core';
+import { outputFile } from 'fs-extra';
+import { Messages } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
+import { prompt } from '@oclif/core/lib/cli-ux/prompt';
+import { flags, SfdxCommand } from '@salesforce/command';
+
 
 Messages.importMessagesDirectory(__dirname);
 
@@ -9,16 +12,12 @@ const messages = Messages.loadMessages('copadoDev', 'function');
 
 export default class CreateFunction extends SfdxCommand {
     public static description = messages.getMessage('new.description');
-
     public static examples = messages.getMessage('new.examples').split(os.EOL);
 
-    public static args = [{ name: 'file' }];
-
+    protected static requiresUsername = true;
     protected static flagsConfig = {
-        // flag with a value (-n, --name=VALUE)
         name: flags.string({
             char: 'n',
-            required: true,
             description: messages.getMessage('new.flags.name.description'),
         }),
         type: flags.enum({
@@ -28,52 +27,85 @@ export default class CreateFunction extends SfdxCommand {
         })
     };
 
-    protected static requiresUsername = true;
 
     public async run(): Promise<AnyJson> {
-        const name = this.flags.name as string;
-        const type = (this.flags.name || 'shell') as string;
-        console.log('type: ', type);
-        const conn = this.org.getConnection();
-        const query = 'Select Name, TrialExpirationDate from Organization';
+        const name = this.flags.name as string || await prompt("Please enter function Name");
+        const type = (this.flags.type || 'shell') as string;
+        const templateFunction = this.templates[type];
 
-        // The type we are querying for
-        interface Organization {
-            Name: string;
-            TrialExpirationDate: string;
-        }
+        await outputFile(`./copado-data/functions/${name}/${this.getFileName(type) }`, templateFunction.script);
+        await outputFile(`./copado-data/functions/${name}/parameter.json`, `${JSON.stringify(templateFunction.parameters, null, 4)}`);
+        await outputFile(`./copado-data/functions/${name}/details.json`, `${JSON.stringify(templateFunction.detail, null, 4)}`);
 
-        // Query the org
-        const result = await conn.query<Organization>(query);
-
-        // Organization will always return one result, but this is an example of throwing an error
-        // The output and --json will automatically be handled for you.
-        if (!result.records || result.records.length <= 0) {
-            throw new SfError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
-        }
-
-        // Organization always only returns one result
-        const orgName = result.records[0].Name;
-        const trialExpirationDate = result.records[0].TrialExpirationDate;
-
-        let outputString = `Hello ${name}! This is org: ${orgName}`;
-        if (trialExpirationDate) {
-            const date = new Date(trialExpirationDate).toDateString();
-            outputString = `${outputString} and I will be around until ${date}!`;
-        }
-        this.ux.log(outputString);
-
-        // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
-        if (this.hubOrg) {
-            const hubOrgId = this.hubOrg.getOrgId();
-            this.ux.log(`My hub org id is: ${hubOrgId}`);
-        }
-
-        if (this.flags.force && this.args.file) {
-            this.ux.log(`You input --force and a file: ${this.args.file as string}`);
-        }
-
-        // Return an object to be displayed with --json
-        return { orgId: this.org.getOrgId(), outputString };
+        return templateFunction;
     }
+
+
+    private getFileName(type: string) : string {
+        if (type === 'shell') return 'script.sh';
+        else if (type === 'nodejs') return 'script.js';
+        else if (type === 'python') return 'script.py';
+        else throw new Error(`Invalid type ${type}`);
+    }
+
+
+    private templates = {
+        shell: {
+            script: `echo Hello $parameter`,
+            detail: {
+                imageName: "copado-function-core:v1",
+                version: "0.1",
+                type: "Custom",
+                timeout: null,
+                options: null,
+                size: "S",
+                flowHandler: "",
+                description: "",
+                callbackType: "",
+                apexClass: ""
+            },
+            parameters: [{
+                "name": "parameter",
+                "value": "world"
+            }]
+        },
+        python: {
+            script: `print("Hello os.environ['parameter']")`,
+            detail: {
+                imageName: "copado-function-core:v1",
+                version: "0.1",
+                type: "Custom",
+                timeout: null,
+                options: null,
+                size: "S",
+                flowHandler: "",
+                description: "",
+                callbackType: "",
+                apexClass: ""
+            },
+            parameters: [{
+                "name": "parameter",
+                "value": "world"
+            }]
+        },
+        nodejs: {
+            script: 'console.log(`Hello ${process.env.parameter}`);',
+            detail: {
+                imageName: "copado-function-core:v1",
+                version: "0.1",
+                type: "Custom",
+                timeout: null,
+                options: null,
+                size: "S",
+                flowHandler: "",
+                description: "",
+                callbackType: "",
+                apexClass: ""
+            },
+            parameters: [{
+                "name": "parameter",
+                "value": "world"
+            }]
+        }
+    };
 }
